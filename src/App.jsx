9f7039478d68f5a39c4171bc7029c2ba3de6d30a
Hamo <medomedo333hamo@gmail.com>
2023-12-27 21:53:48 +0200

    Create files  src/App.css         src/App.jsx         src/assets/         src/blog/         src/components/         src/home/         src/index.jsx         src/products.json         src/utilis/

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css'
+import { Outlet } from "react-router-dom";
+import NavItems from "./components/NavItems";
+
+function App() {
+  return (
+    <>
+    <NavItems />
+    <Outlet />
+    </>
+  );
+}
+
+export default App;
