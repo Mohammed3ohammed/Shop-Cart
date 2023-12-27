579f3de988cd97579d0d77ba83479d5a4aec592b
Hamo <medomedo333hamo@gmail.com>
2023-12-28 00:43:01 +0200

    Upload the section Banner

---
+++ src/home/Banner.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import productData from '../products.json'
+import { Link } from 'react-router-dom';
+
+const title = (
+    <h2>Serch Your One From <span>Thousand</span> Products</h2>
+)
+const desc = "We have the largest collection of products"
+const bannerList = [
+    {
+    iconName: "icofont-users-alt-4",
+    text: "1.5 Million Customers", 
+    },
+    {
+    iconName: "icofont-notification",
+    text: "More then 2000 Marchent",
+    },
+    {
+    iconName: "icofont-globe",
+    text: "Buy Anything Online",
+    },
+    ];
+
+const Banner = () => {
+
+    const [searchInput, setsearchInput] = useState("");
+    const [filteredProducts, setfilteredProducts] = useState(productData);
+    // console.log(productsData)
+
+    // Function Search
+    const handleSearch = e => {
+    const searchTerm = e.target.value
+    setsearchInput(searchTerm)
+
+    // filtering products based on search
+    const filtered =  productData.filter((product) => product.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+    setfilteredProducts(filtered)
+    }
+
+  return (
+    <div className="banner-section style-4">
+        <div className="container">
+            <div className="banner-content">
+            {title}
+            <form>
+                <input
+                 type="text"
+                  name="search"
+                   id='search'
+                    placeholder="Search your products"
+                     value={searchInput} 
+                     onChange={handleSearch}
+                     /> 
+                     <button type='submit'>
+                        <i className="icofont-search"></i>
+                        </button>
+            </form>
+            <p>{desc}</p>
+            <ul className='lab-ul'>
+                {
+                searchInput && filteredProducts.map((product, i) =>  
+                <li key={i}>
+                    <Link to={`/shop/${product.id}`}>{product.name}</Link>
+                </li>
+                )
+                }
+            </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
