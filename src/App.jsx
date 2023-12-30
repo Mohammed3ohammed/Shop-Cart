5892ad3e49ab27c5c978a05e329d69f039fe7563
Hamo <medomedo333hamo@gmail.com>
2023-12-30 23:00:35 +0200

    Upload the Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import './App.css'
 import { Outlet } from "react-router-dom";
 import NavItems from "./components/NavItems";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
     <NavItems />
     <Outlet />
+    <Footer />
     </>
   );
 }
