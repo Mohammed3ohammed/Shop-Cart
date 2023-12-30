4cb9c5376ddd257dd5903387ffbb3415e72c64f8
Hamo <medomedo333hamo@gmail.com>
2023-12-30 20:54:24 +0200

    Upload the section AppSection

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -5,6 +5,7 @@ import CategoryShowCase from './CategoryShowCase'
 import Register from './Register'
 import LocationSprade from './LocationSprade'
 import AboutUs from './AboutUs'
+import AppSection from './AppSection'
 
 
 const Home = () => {
@@ -16,6 +17,7 @@ const Home = () => {
       <Register />
       <LocationSprade />
       <AboutUs />
+      <AppSection />
     </div>
   )
 }
