5f6095806620d45a222e703d4038428f724802d9
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2024-03-13 02:09:44 +0200

    Added new files

--- src/index.jsx
+++ src/index.jsx
@@ -1,32 +1,79 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './index.css'
+
+// import 'swiper/css';
+
+// // bootstrap css
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+// // fonts and icons
+// import '././assets/css/icofont.min.css';
+// import '././assets/css/animate.css';
+// import '././assets/css/style.min.css';
+
+// import Home from './home/Home';
+// import Blog from './blog/Blog';
+// import Shop from './shop/Shop';
+// import SingleProduct from './shop/SingleProduct';
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+
+//     children: [
+//       {
+//          path: "/", element: <Home /> 
+//         },
+//       {
+//          path: "/blog", element: <Blog />
+//     },
+//     {
+//       path: "/shop", element: <Shop />
+//     },
+//     {
+//       path: 'shop/:id', element: <SingleProduct  />
+//     }
+//     ]
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <RouterProvider router={router} />
+
+// );
+
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Home from './home/Home';
-import Blog from './blog/Blog';
-import Shop from './shop/Shop';
-import SingleProduct from './shop/SingleProduct';
 import './index.css';
 
 import 'swiper/css';
-
-// bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-// fonts and icons
 import './assets/css/icofont.min.css';
 import './assets/css/animate.css';
 import './assets/css/style.min.css';
 
+import Home from './home/Home';
+import Blog from './blog/Blog';
+import Shop from './shop/Shop';
+import SingleProduct from './shop/SingleProduct';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
-    <App />
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/blog" element={<Blog />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/shop/:id" element={<SingleProduct />} />
+      <Route path="/" element={<App/>}>
+        <Route index element={<Home />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="shop/:id" element={<SingleProduct />} />
+      </Route>
     </Routes>
   </Router>
 );
